00b45ede7511f19e3a48f70669ecd0ac8caaa94f
Benjamin König <be.koenig@lvm.de>
2025-05-19 11:18:31 +0200

    refactor: extracted inline class to DatabaseConnectionSourceAdapter
    - added tests

--- trymigrate-core/src/main/java/io/github/bekoenig/trymigrate/core/internal/catalog/CatalogFactory.java
+++ trymigrate-core/src/main/java/io/github/bekoenig/trymigrate/core/internal/catalog/CatalogFactory.java
@@ -2,21 +2,19 @@ package io.github.bekoenig.trymigrate.core.internal.catalog;
 
 import org.junit.platform.commons.util.ClassLoaderUtils;
 import schemacrawler.schema.Catalog;
-import schemacrawler.schemacrawler.*;
+import schemacrawler.schemacrawler.LimitOptions;
+import schemacrawler.schemacrawler.LoadOptions;
+import schemacrawler.schemacrawler.SchemaCrawlerOptionsBuilder;
 import schemacrawler.tools.utility.SchemaCrawlerUtility;
-import us.fatehi.utility.datasource.DatabaseConnectionSource;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.lang.reflect.Proxy;
 import java.sql.Connection;
-import java.util.function.Consumer;
 import java.util.logging.LogManager;
 
 public class CatalogFactory {
 
-    private static final String LOG_PROPERTIES =
-            "io/github/bekoenig/trymigrate/core/internal/catalog/log.properties";
+    private static final String LOG_PROPERTIES = "io/github/bekoenig/trymigrate/core/internal/catalog/log.properties";
 
     static {
         try (InputStream is = ClassLoaderUtils.getClassLoader(CatalogFactory.class)
@@ -37,44 +35,10 @@ public class CatalogFactory {
     }
 
     public Catalog crawl(Connection connection) {
-        return SchemaCrawlerUtility.getCatalog(wrap(connection),
+        return SchemaCrawlerUtility.getCatalog(new DatabaseConnectionSourceAdapter(connection),
                 SchemaCrawlerOptionsBuilder.newSchemaCrawlerOptions()
                         .withLimitOptions(limitOptions)
                         .withLoadOptions(loadOptions));
-    }
-
-    private DatabaseConnectionSource wrap(Connection connection) {
-        return new DatabaseConnectionSource() {
-            @Override
-            public Connection get() {
-                return (Connection) Proxy.newProxyInstance(
-                        ClassLoaderUtils.getClassLoader(Connection.class),
-                        new Class<?>[]{Connection.class},
-                        (proxy, method, args) -> {
-                            if (method.getName().equals("close")) {
-                                return null;
-                            }
-
-                            return method.invoke(connection, args);
-                        });
-            }
-
-            @Override
-            public void close() {
-                // Noop
-            }
-
-            @Override
-            public boolean releaseConnection(Connection connection) {
-                // Noop
-                return true;
-            }
-
-            @Override
-            public void setFirstConnectionInitializer(Consumer<Connection> connectionInitializer) {
-                // Noop
-            }
-        };
     }
 
 }
