32001a4924f0f5fe401e6517b6998ece8ac85c13
Benjamin König <be.koenig@lvm.de>
2025-05-27 19:52:50 +0200

    docs: added some public interface documentation

--- trymigrate-core/src/main/java/io/github/bekoenig/trymigrate/core/plugin/TrymigrateBeanProvider.java
+++ trymigrate-core/src/main/java/io/github/bekoenig/trymigrate/core/plugin/TrymigrateBeanProvider.java
@@ -11,7 +11,7 @@ import java.util.Optional;
 public interface TrymigrateBeanProvider {
 
     /**
-     * Gets all beans for class ordered.
+     * Gets all compatible beans for class ordered.
      *
      * @param clazz class
      * @return list of beans
@@ -19,11 +19,42 @@ public interface TrymigrateBeanProvider {
      */
     <T> List<T> all(Class<T> clazz);
 
+    /**
+     * Gets exactly one compatible bean for class.
+     *
+     * @throws IllegalStateException on missing or multiple beans
+     * @param clazz class
+     * @return one bean
+     * @param <T> type of bean
+     */
     <T> T one(Class<T> clazz);
 
+    /**
+     * Gets one or no compatible bean for class.
+     *
+     * @throws IllegalStateException on multiple beans
+     * @param clazz class
+     * @return one or no bean
+     * @param <T> type of bean
+     */
     <T> Optional<T> findOne(Class<T> clazz);
 
+    /**
+     * Gets exactly one compatible bean for class. On multiple beans, the highest order will be used.
+     *
+     * @throws IllegalStateException on no bean
+     * @param clazz class
+     * @return one bean
+     * @param <T> type of bean
+     */
     <T> T first(Class<T> clazz);
 
+    /**
+     * Gets one or no compatible bean for class. On multiple beans, the highest order will be used.
+     *
+     * @param clazz class
+     * @return one or no bean
+     * @param <T> type of bean
+     */
     <T> Optional<T> findFirst(Class<T> clazz);
 }
