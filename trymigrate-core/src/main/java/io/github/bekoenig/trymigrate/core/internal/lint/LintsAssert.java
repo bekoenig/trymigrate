79eb98a064883c3076757e30533d7d9f0f837c20
Benjamin König <koenig87@googlemail.com>
2025-11-02 21:21:35 +0100

    feat: added details on failing test due lints errors

--- trymigrate-core/src/main/java/io/github/bekoenig/trymigrate/core/internal/lint/LintsAssert.java
+++ trymigrate-core/src/main/java/io/github/bekoenig/trymigrate/core/internal/lint/LintsAssert.java
@@ -1,5 +1,9 @@
 package io.github.bekoenig.trymigrate.core.internal.lint;
 
+import io.github.bekoenig.trymigrate.core.lint.TrymigrateExcludeLint;
+import io.github.bekoenig.trymigrate.core.lint.TrymigrateSuppressLint;
+import io.github.bekoenig.trymigrate.core.lint.config.TrymigrateLintersConfigurer;
+import io.github.bekoenig.trymigrate.core.plugin.TrymigrateBean;
 import schemacrawler.tools.lint.Lint;
 import schemacrawler.tools.lint.LintSeverity;
 import schemacrawler.tools.lint.Lints;
@@ -27,11 +31,31 @@ public class LintsAssert {
             return;
         }
 
-        throw new AssertionError(MessageFormat.format("Fix or accept lints: {0}{1}",
-                System.lineSeparator(),
+        throw new AssertionError(MessageFormat.format("""
+                Some lints have exceeded the quality threshold ({0}):
+                
+                {1}
+                
+                What can you do?
+                
+                For a new migration:
+                  Try to improve it
+                For an already applied migration, or if this rule needs to be ignored once for a valid reason:
+                  Use @{2} at the method level to suppress the rule for the failing test method
+                For general or partial rejection of this rule:
+                  Use @{3} at the class level to exclude this rule for the entire test class
+                
+                Note: If you want to customize the lint profile (e.g. severity, affected database objects, ...), add
+                a field annotated with @{4} to test class or a custom plugin and implement a {5}
+                """,
+                failOn.name(),
                 assertLints.stream()
                         .map(x -> x.getLinterId() + ": " + x)
-                        .collect(Collectors.joining(System.lineSeparator()))));
+                        .collect(Collectors.joining(System.lineSeparator())),
+                TrymigrateSuppressLint.class.getSimpleName(),
+                TrymigrateExcludeLint.class.getSimpleName(),
+                TrymigrateBean.class.getSimpleName(),
+                TrymigrateLintersConfigurer.class.getSimpleName()));
     }
 
     protected static boolean hasOrExceedsSeverity(Lint<? extends Serializable> lint, LintSeverity threshold) {
