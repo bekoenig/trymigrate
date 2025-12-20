c92e4b5aa8e14b7e77dd1eefb2945f87180f4d05
Benjamin König <koenig87@googlemail.com>
2025-12-20 21:50:47 +0100

    docs: fixed reference in TrymigrateSuppressLint

--- trymigrate-core/src/main/java/io/github/bekoenig/trymigrate/core/lint/TrymigrateSuppressLint.java
+++ trymigrate-core/src/main/java/io/github/bekoenig/trymigrate/core/lint/TrymigrateSuppressLint.java
@@ -1,13 +1,11 @@
 package io.github.bekoenig.trymigrate.core.lint;
 
-import io.github.bekoenig.trymigrate.core.Trymigrate;
-
 import java.lang.annotation.*;
 
 /**
  * Marker annotation to suppress lints for test method. Use this annotation to allowlist lints which break the
- * quality gate of {@link Trymigrate#failOn()}. The lints are not removed. They are still included in the report and in
- * the method parameter set for further asserts.
+ * quality gate of {@link TrymigrateAssertLints#failOn()}. The lints are not removed. They are still included in the
+ * report and in the method parameter set for further asserts.
  * <p>
  * Supports regex to accept multiple lints using single annotation.
  */
