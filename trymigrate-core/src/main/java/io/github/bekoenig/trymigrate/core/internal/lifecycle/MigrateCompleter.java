4e441da41b4afa9164312ef07907b4c0160aa1b6
Benjamin König <koenig87@googlemail.com>
2026-01-24 14:06:35 +0100

    fix: avoided NPE in completer on failed initialization

--- trymigrate-core/src/main/java/io/github/bekoenig/trymigrate/core/internal/lifecycle/MigrateCompleter.java
+++ trymigrate-core/src/main/java/io/github/bekoenig/trymigrate/core/internal/lifecycle/MigrateCompleter.java
@@ -1,6 +1,7 @@
 package io.github.bekoenig.trymigrate.core.internal.lifecycle;
 
 import io.github.bekoenig.trymigrate.core.internal.StoreSupport;
+import io.github.bekoenig.trymigrate.core.internal.migrate.MigrateProcessor;
 import org.junit.jupiter.api.extension.ExtensionContext;
 import org.junit.jupiter.api.extension.TestInstancePreDestroyCallback;
 
@@ -8,7 +9,10 @@ public class MigrateCompleter implements TestInstancePreDestroyCallback {
 
     @Override
     public void preDestroyTestInstance(ExtensionContext extensionContext) {
-        StoreSupport.getMigrateProcessor(extensionContext).finish();
+        MigrateProcessor migrateProcessor = StoreSupport.getMigrateProcessor(extensionContext);
+        if (migrateProcessor != null) {
+            migrateProcessor.finish();
+        }
     }
 
 }
